5a34d2c774d5a97881a74921b568e046bd2c6649
Dionatas Santos Brito <80075307+DsBrito@users.noreply.github.com>
2024-10-06 02:00:26 -0300

    🚧 add some skill images

---
+++ src/components/class/CardClassItem.vue
@@ -0,0 +1,102 @@
+<template>
+  <q-page class="test">
+    <!-- <q-page class="q-pa-md test"> -->
+    <img src="../../assets/image/icons/page_breakg.png" />
+
+    <h5><strong> HISTORY</strong></h5>
+
+    {{ character.history }}
+    <img src="../../assets/image/icons/page_breakg.png" />
+
+    <q-card v-if="character">
+      <!-- class="q-mb-md"> -->
+
+      <!--CLASSE-->
+      <!-- <img src="character.classUrl" /> -->
+      <!-- SKILLS-->
+
+      <Skills
+        :class="character.classUrl"
+        :skillBasic="character.skillBasic"
+        :skillSingle="character.skillSingle"
+        :skillBuff="character.skillBuff"
+        :skillRanger="character.skillRanger"
+      ></Skills>
+
+      <!-- <q-card-section>
+        <div class="row items-center">
+          <div class="col">
+            <h4>{{ character.class }}</h4>
+            <p>{{ character.type }}</p>
+          </div>
+          <div class="col text-right">
+            <q-badge color="primary" label="{{ character.role }}" />
+          </div>
+        </div>
+      </q-card-section> -->
+
+      <!-- <q-card-section>
+        <q-list>
+          <q-item>
+            <q-item-section>
+              <q-item-label>{{ character.description }}</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-item>
+            <q-item-section>
+              <q-item-label
+                ><strong>História:</strong>
+                {{ character.history }}</q-item-label
+              >
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-card-section> -->
+    </q-card>
+  </q-page>
+  <!--CLASSE-->
+  <img src="../../assets/image/icons/page_breakg.png" />
+</template>
+
+<script>
+import Skills from 'components/class/SkillCarousel.vue';
+
+export default {
+  components: {
+    Skills,
+  },
+  name: 'CardClassItem',
+  props: {
+    character: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.q-card {
+  border-radius: 10px;
+  box-shadow: 0px 0px rgba(0, 0, 0, 0);
+}
+.q-card-section h4 {
+  font-weight: bold;
+  margin-bottom: 5px;
+}
+.q-card-section p {
+  color: #666;
+}
+.test {
+  font-family: 'Poppins', sans-serif;
+  font-size: 18px;
+  text-align: justify;
+  width: 70%;
+  margin: 0 auto;
+  padding: 16px;
+}
+.resized-img {
+  width: 200px;
+  height: auto;
+}
+</style>
---
+++ src/components/class/SkillCarousel.vue
@@ -0,0 +1,125 @@
+<template>
+  <!-- <img :src="props.class" class="" /> -->
+
+  <h6 class="text-center"><strong>SKILLS | LINKS | ATTIBUTES </strong></h6>
+
+  <div class="class-skill-wrapper">
+    <AttributesTable></AttributesTable>
+
+    <!-- <img :src="props.class" class="class-image" /> -->
+    <q-carousel
+      animated
+      v-model="slide"
+      arrows
+      navigation
+      infinite
+      swipeable
+      control-color="white"
+      class="carousel-custom no-padding"
+    >
+      <!-- Skill Basic Slide -->
+      <q-carousel-slide :name="1" class="no-padding">
+        <q-card class="skill-card">
+          <q-card-section class="no-padding">
+            <img
+              :src="props.skillBasic"
+              alt="Skill Basic"
+              class="skill-image"
+            />
+          </q-card-section>
+        </q-card>
+      </q-carousel-slide>
+
+      <!-- Skill Single Slide -->
+      <q-carousel-slide :name="2" class="no-padding">
+        <q-card class="skill-card">
+          <q-card-section class="no-padding">
+            <img
+              :src="props.skillSingle"
+              alt="Skill Single"
+              class="skill-image"
+            />
+          </q-card-section>
+        </q-card>
+      </q-carousel-slide>
+
+      <!-- Skill Buff Slide -->
+      <q-carousel-slide :name="3" class="no-padding">
+        <q-card class="skill-card">
+          <q-card-section class="no-padding">
+            <img :src="props.skillBuff" alt="Skill Buff" class="skill-image" />
+          </q-card-section>
+        </q-card>
+      </q-carousel-slide>
+
+      <!-- Skill Ranger Slide -->
+      <q-carousel-slide :name="4" class="no-padding">
+        <q-card class="skill-card">
+          <q-card-section class="no-padding">
+            <img
+              :src="props.skillRanger"
+              alt="Skill Ranger"
+              class="skill-image"
+            />
+          </q-card-section>
+        </q-card>
+      </q-carousel-slide>
+    </q-carousel>
+    <q-img
+      src="../../assets/image/boss/icons/rightArrow.png"
+      class="resized-img"
+      :ratio="1"
+    />
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+import AttributesTable from 'components/class/AttributesTable.vue';
+
+const slide = ref(1);
+
+const props = defineProps<{
+  class: string;
+  skillBasic: string;
+  skillSingle: string;
+  skillBuff: string;
+  skillRanger: string;
+}>();
+</script>
+
+<style scoped>
+.class-skill-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.class-image {
+  max-width: 200px; /* Ajuste o tamanho da imagem da classe */
+  height: auto;
+  /* margin-right: 20px; Espaçamento entre a imagem e o carrossel */
+}
+
+.carousel-custom {
+  max-width: 350px;
+  flex-grow: 1;
+}
+
+.skill-card {
+  overflow: hidden;
+  box-shadow: 0 0px 0px rgba(0, 0, 0, 0.1);
+  height: 100%;
+}
+
+.skill-image {
+  width: 100%;
+  height: 100%;
+  /* object-fit: cover; */
+  border-radius: 8px;
+}
+
+.resized-img {
+  width: 200px;
+  height: auto;
+}
+</style>
