1a276ad3558ac75a6628a6ac3385585dd338f4c6
Dionatas Santos Brito <80075307+DsBrito@users.noreply.github.com>
2024-10-06 02:02:13 -0300

    🐛 fix model files

---
+++ src/model/class/characterHistory.ts
@@ -0,0 +1,33 @@
+export const HISTORY = [
+  {
+    id: 1,
+    history:
+      'Humans emerged in Shaiya during the event known as the Second Supernatural Eclipse, which marked the birth of new life in the world of Teos Epeiros. Following the rebirth of the split souls of the goddess Etain and the emergence of two new goddesses—the Goddess of Light and the Goddess of Darkness—the world continued to evolve. During this second eclipse, humans emerged from the oceans, seeking more lands to expand their race. They quickly settled in Teos Epeiros and formed a prosperous kingdom. The humans encountered the Elves and together, under the guidance of the Goddess of Light, they formed an alliance: the Alliance of Light. The arrival of the humans was not only a milestone in territorial growth, but also a time when their culture and power grew alongside their Elven allies, playing a central role in the ongoing struggle against the Union of Rage, comprised of the Vails and Nordeins. Thus, humans became one of the main forces in Shaiya, representing the faction of light and fighting the darkness in an epic battle for control and survival in the world of Teos.',
+  },
+  {
+    id: 2,
+    history:
+      "After Etain's tragic death, the Dumianas were almost completely destroyed. Only two Dumianas survived: Proton and Eustatin. These last two members of the original race went their separate ways. Proton, embittered by the Dumianas' mistakes and regretting the goddess' death, decided to live in solitude. In his solitude, Proton carved a statue in a tree, in the image of the goddess Etain, to honor her and try to redeem himself for the sins of his race. He poured all his devotion and repentance into this sculpture, trying to imitate Etain's perfection. One day, supernaturally, the wooden sculpture came to life, absorbing the blood of the earth that still carried traces of the goddess Etain's divinity. The statue transformed into a living woman, a beautiful and powerful being who soon united with Proton. From this union, the Elves were born. These new beings inherited the beauty, wisdom and connection with nature, reflecting the harmony that Proton tried to restore.",
+  },
+  {
+    id: 3,
+    history:
+      "The Nordein were the first race created by the goddess Etain to populate Teos Epeiros, but, because they were considered imperfect and brutal, they were banished underground by the goddess herself. During millennia underground, they evolved and became even stronger and more resilient. After Etain's death, the Nordein emerged from the darkness as the Death Eaters, driven by resentment and a desire for revenge against the surface races.",
+  },
+  {
+    id: 4,
+    history:
+      'The Vail were born when Eustatin, a dumian rejected by Proton, gave birth to her son in the darkness of southern Teos, creating a new race shaped by loneliness and rage. They developed into a society of their own, far from the conflicts of the other races. Thus, the Vail were born of division and betrayal, defined by a deep resentment and desire for revenge, seeking power and retribution against those who betrayed them.',
+  },
+  {
+    id: 5,
+    history:
+      'In the rugged lands of the Union of Fury, where the Vail and the Deatheaters fought against the might of the Alliance, the Dark Goddess sought new strength to turn the tide of war. Deep in meditation, she reached out to the spirit of Teos, seeking allies long forgotten by the outside world. In response, the Spirit Bear Clan emerged from the shadows of the southern forests. From this hidden lineage emerged the Monks of the Spirit Bear Clan, fierce fighters who wielded strength and ancient spiritual power. These warriors were shaped by a dual philosophy of power: they were masters of both brute force and agile martial prowess. Their bodies, hardened by nature, channeled the power of the earth through their two distinct combat styles. Armed with staves that delivered crushing blows and gauntlets that allowed them to strike quickly and lethally, the monks were unstoppable in close combat. ',
+  },
+  ,
+  {
+    id: 6,
+    history:
+      'Meanwhile, in the north, within the verdant lands of the Alliance of Light, the Spirit Bear Clan revealed another facet of their ancient legacy. From the peaceful forests of their homeland, the Druids of the Spirit Bear Clan emerged, answering the call of the Goddess of Light. Though they shared the same lineage as their brethren to the south, these druids followed a path of peace, balance, and harmony. Skilled martial artists, they also wielded staves and gauntlets in battle, but their connection to nature allowed them to strike with grace and precision, blending power with the fluidity of the forest’s movements. Known for their meditative practices and their deep understanding of the spiritual forces that governed Teos, the druids sought to bring balance and protection to the lands under their care.',
+  },
+];
