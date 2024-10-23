c7a806671eb25bd5fd2669a09e69385d0fd195fa
Dionatas Santos Brito <80075307+DsBrito@users.noreply.github.com>
2024-10-23 01:03:17 -0300

    :sparkles: update  skill carousel

---
+++ src/model/class/characterVideo.ts
@@ -0,0 +1,259 @@
+export const VIDEO = [
+  {
+    //fighter-warrior
+    id: 1,
+    video: [
+      {
+        type: 'PVP',
+        links: [
+          {
+            name: 'Stadium',
+            tumb: require('../../assets/image/class/human/fight/video/pvp/stadium.png'),
+            link: 'https://www.youtube.com/watch?v=5QBwVT2R8X0&t=35s&ab_channel=Kiro4ever',
+          },
+          {
+            name: 'Vulcanos',
+            tumb: require('../../assets/image/class/human/fight/video/pvp/vulcanos.png'),
+            link: 'https://www.youtube.com/watch?v=jidGOjuklQk&ab_channel=LoTTek%233233',
+          },
+          {
+            name: 'Kanos Illium',
+            tumb: require('../../assets/image/class/human/fight/video/pvp/kanosIllium.png'),
+            link: 'https://www.youtube.com/watch?v=HEYqwhKFkJU&ab_channel=Giordy',
+          },
+          {
+            name: 'DD1',
+            tumb: require('../../assets/image/class/human/fight/video/pvp/dd1.png'),
+            link: 'https://www.youtube.com/watch?v=87RmNzTCtc0&t=2s&ab_channel=Incarnation',
+          },
+          {
+            name: 'Iris Invasion',
+            tumb: require('../../assets/image/class/human/fight/video/pvp/irisIvasion.png'),
+            link: 'https://www.youtube.com/watch?v=ugaG6LVznes&ab_channel=LoTTek%233233',
+          },
+          {
+            name: 'D-Water',
+            tumb: require('../../assets/image/class/human/fight/video/pvp/dwater.png'),
+            link: 'https://www.youtube.com/watch?v=DTc44Qulgog&ab_channel=Incarnation',
+          },
+        ],
+      },
+      {
+        type: '1VS1',
+        links: [
+          {
+            name: 'Kitty vs RocknRoll',
+            tumb: require('../../assets/image/class/human/fight/video/1vs1/kitty_vs_rock.png'),
+            link: 'https://www.youtube.com/watch?v=0tIk9cRb89w&ab_channel=Incarnation',
+          },
+          {
+            name: 'Incarnation vs Susanoo',
+            tumb: require('../../assets/image/class/human/fight/video/1vs1/incarnation_vs_susanoo.png'),
+            link: 'https://www.youtube.com/watch?v=Cl8W5oGtCWY&ab_channel=Incarnation',
+          },
+          {
+            name: 'Gyutaro vs Kitty',
+            tumb: require('../../assets/image/class/human/fight/video/1vs1/gyutaro_vs_kitty.png'),
+            link: 'https://youtube.com/watch?v=TmOesYps_RQ&ab_channel=Kiro4ever',
+          },
+          {
+            name: 'boobs vs Godjr',
+            tumb: require('../../assets/image/class/human/fight/video/1vs1/boobs_vs_godjr.png'),
+            link: 'https://www.youtube.com/watch?v=4ibYlAX1izM&ab_channel=Incarnation',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    //defender-guardian
+    id: 2,
+    video: [
+      {
+        type: 'PVP',
+        links: [
+          {
+            name: 'The Mist',
+            tumb: require('../../assets/image/class/elf/ranger/video/pvp/ranger_mist.png'),
+            link: 'https://www.youtube.com/watch?v=RkLlXJ31C-8&ab_channel=Saty',
+          },
+        ],
+      },
+      {
+        type: '1VS1',
+        links: [
+          {
+            name: '1VS1 Fight',
+            tumb: require('../../assets/image/class/elf/ranger/video/1vs1/ranger_vs_fight.png'),
+            link: 'https://www.youtube.com/watch?v=8y2B9-koIJM&ab_channel=Saty',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    //priest-oracle
+    id: 3,
+    video: [
+      {
+        type: 'PVP',
+        links: [
+          {
+            name: 'The Mist',
+            tumb: require('../../assets/image/class/elf/ranger/video/pvp/ranger_mist.png'),
+            link: 'https://www.youtube.com/watch?v=RkLlXJ31C-8&ab_channel=Saty',
+          },
+        ],
+      },
+      {
+        type: '1VS1',
+        links: [
+          {
+            name: '1VS1 Fight',
+            tumb: require('../../assets/image/class/elf/ranger/video/1vs1/ranger_vs_fight.png'),
+            link: 'https://www.youtube.com/watch?v=8y2B9-koIJM&ab_channel=Saty',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    //ranger-sin
+    id: 4,
+    video: [
+      {
+        type: 'PVP',
+        links: [
+          {
+            name: 'The Mist',
+            tumb: require('../../assets/image/class/elf/ranger/video/pvp/ranger_mist.png'),
+            link: 'https://www.youtube.com/watch?v=RkLlXJ31C-8&ab_channel=Saty',
+          },
+          {
+            name: 'Locus Graveyard',
+            tumb: require('../../assets/image/class/elf/ranger/video/pvp/ranger_locus.png'),
+            link: 'https://www.youtube.com/watch?v=xPC0waGAtnQ&t=309s&ab_channel=Saty',
+          },
+          {
+            name: 'Kanos Illium',
+            tumb: require('../../assets/image/class/elf/ranger/video/pvp/ranger_kanos.png'),
+            link: 'https://www.youtube.com/watch?v=LQycFUwCRjc&t=4s&ab_channel=Saty',
+          },
+          {
+            name: 'Vulcano Island',
+            tumb: require('../../assets/image/class/elf/ranger/video/pvp/ranger_vi.png'),
+            link: 'https://www.youtube.com/watch?v=oIXsmpErbHA&ab_channel=Saty',
+          },
+          {
+            name: 'Older Jungle',
+            tumb: require('../../assets/image/class/elf/ranger/video/pvp/ranger_jungle.png'),
+            link: 'https://www.youtube.com/watch?v=0BGVNvvoBzE&t=107s&ab_channel=Saty',
+          },
+        ],
+      },
+      {
+        type: '1VS1',
+        links: [
+          {
+            name: '1VS1 Fight',
+            tumb: require('../../assets/image/class/elf/ranger/video/1vs1/ranger_vs_fight.png'),
+            link: 'https://www.youtube.com/watch?v=8y2B9-koIJM&ab_channel=Saty',
+          },
+          {
+            name: '1VS1 Ranger',
+            tumb: require('../../assets/image/class/elf/ranger/video/1vs1/ranger_vs_ranger.png'),
+            link: 'https://www.youtube.com/watch?v=z2wRkCSiMAk&ab_channel=Saty',
+          },
+          {
+            name: '1VS1 Archer',
+            tumb: require('../../assets/image/class/elf/ranger/video/1vs1/ranger_vs_archer.png'),
+            link: 'https://www.youtube.com/watch?v=HKp-XSy8oGw&ab_channel=Saty',
+          },
+          {
+            name: '1VS2 Archer',
+            tumb: require('../../assets/image/class/elf/ranger/video/1vs1/ranger_vs_two_archer.png'),
+            link: 'https://www.youtube.com/watch?v=TaGO5cd4QZU&ab_channel=Saty',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    //archer-hunter
+    id: 5,
+    video: [
+      {
+        type: 'PVP',
+        links: [
+          {
+            name: 'The Mist',
+            tumb: require('../../assets/image/class/elf/ranger/video/pvp/ranger_mist.png'),
+            link: 'https://www.youtube.com/watch?v=RkLlXJ31C-8&ab_channel=Saty',
+          },
+        ],
+      },
+      {
+        type: '1VS1',
+        links: [
+          {
+            name: '1VS1 Fight',
+            tumb: require('../../assets/image/class/elf/ranger/video/1vs1/ranger_vs_fight.png'),
+            link: 'https://www.youtube.com/watch?v=8y2B9-koIJM&ab_channel=Saty',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    //mage-pagao
+    id: 6,
+    video: [
+      {
+        type: 'PVP',
+        links: [
+          {
+            name: 'The Mist',
+            tumb: require('../../assets/image/class/elf/ranger/video/pvp/ranger_mist.png'),
+            link: 'https://www.youtube.com/watch?v=RkLlXJ31C-8&ab_channel=Saty',
+          },
+        ],
+      },
+      {
+        type: '1VS1',
+        links: [
+          {
+            name: '1VS1 Fight',
+            tumb: require('../../assets/image/class/elf/ranger/video/1vs1/ranger_vs_fight.png'),
+            link: 'https://www.youtube.com/watch?v=8y2B9-koIJM&ab_channel=Saty',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    //druid-monkey
+    id: 7,
+    video: [
+      {
+        type: 'PVP',
+        links: [
+          {
+            name: 'The Mist',
+            tumb: require('../../assets/image/class/elf/ranger/video/pvp/ranger_mist.png'),
+            link: 'https://www.youtube.com/watch?v=RkLlXJ31C-8&ab_channel=Saty',
+          },
+        ],
+      },
+      {
+        type: '1VS1',
+        links: [
+          {
+            name: '1VS1 Fight',
+            tumb: require('../../assets/image/class/elf/ranger/video/1vs1/ranger_vs_fight.png'),
+            link: 'https://www.youtube.com/watch?v=8y2B9-koIJM&ab_channel=Saty',
+          },
+        ],
+      },
+    ],
+  },
+];
