91e9de221f5c4de71c1b6f5f0c22db51c33df549
Dionatas Santos Brito <80075307+DsBrito@users.noreply.github.com>
2024-11-12 00:31:32 -0300

    ✨ update about me page

--- src/pages/AboutMePage.vue
+++ src/pages/AboutMePage.vue
@@ -1,46 +1,29 @@
 <template>
-  <div class="q-pa-md">
-    <q-carousel
-      animated
-      v-model="slide"
-      navigation
-      infinite
-      :autoplay="autoplay"
-      arrows
-      transition-prev="slide-right"
-      transition-next="slide-left"
-      @mouseenter="autoplay = false"
-      @mouseleave="autoplay = true"
-    >
-      <q-carousel-slide
-        :name="1"
-        img-src="https://cdn.quasar.dev/img/mountains.jpg"
-      />
-      <q-carousel-slide
-        :name="2"
-        img-src="https://cdn.quasar.dev/img/parallax1.jpg"
-      />
-      <q-carousel-slide
-        :name="3"
-        img-src="https://cdn.quasar.dev/img/parallax2.jpg"
-      />
-      <q-carousel-slide
-        :name="4"
-        img-src="https://cdn.quasar.dev/img/quasar.jpg"
-      />
-    </q-carousel>
+  <div class="container">
+    <q-img
+      class="img"
+      :src="require('../assets/image/aboutme/image-removebg-preview.png')"
+    />
+    <div class="drop-img hei"></div>
   </div>
 </template>
 
-<script>
-import { ref } from 'vue';
+<script setup lang="ts"></script>
 
-export default {
-  setup() {
-    return {
-      slide: ref(1),
-      autoplay: ref(true),
-    };
-  },
-};
-</script>
+<style>
+.img {
+  width: 20%;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  /* background-image: url('../assets/image/class/background/sq_forum_bg.jpg'); */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  min-height: 100vh;
+}
+</style>
