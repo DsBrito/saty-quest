0b6d1b74127d648f91542cd30626b1c89a3e4f9d
Dionatas Santos Brito <80075307+DsBrito@users.noreply.github.com>
2024-11-12 00:33:52 -0300

    ✨ update model componet

--- src/components/models.ts
+++ src/components/models.ts
@@ -1,15 +1,14 @@
-export interface Todo {
-  id: number;
-  content: string;
-}
+// export interface Todo {
+//   id: number;
+//   content: string;
+// }
 
-export interface Meta {
-  totalCount: number;
-}
-
+// export interface Meta {
+//   totalCount: number;
+// }
 export interface EssentialLinkProps {
   title: string;
-  caption?: string;
-  link?: string;
-  icon?: string;
+  caption: string;
+  link: string | (() => void);
+  icon: string;
 }
