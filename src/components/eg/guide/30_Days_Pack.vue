8d4fa95356966b97ef8c0e8e85350abc26430e50
Dionatas Santos Brito <80075307+DsBrito@users.noreply.github.com>
2024-11-25 01:15:47 -0300

    ✨ update guide component

--- src/components/eg/guide/30_Days_Pack.vue
+++ src/components/eg/guide/30_Days_Pack.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container text-justify">
     <h1 class="title">7~30 DAYS ITENS</h1>
     <h6 class="subtitle text-center">NPC: <strong>Mysterious Woman</strong></h6>
     <q-img
@@ -9,14 +9,14 @@
 
     <div class="itens-wrapper">
       <!-- Primeira linha (Dispel Potion) -->
-      <div class="dispel-potion">
+      <div>
         <div class="item">
           <q-img
-            class="itens-img dispel-potion-img"
+            class="itens-img img-icon"
             src="../../../assets/image/eg/guide/30_days_packet/dispel_potion.png"
           />
           <div class="text">
-            <span class="name">Dissipadora Infinita 7d</span>
+            <span class="name">Infinite Potions 7d</span>
             <p class="description">
               Permanent version of the following elixirs: <br />
               Dispel Elixir, Abolishing Elixir,Blessing Elixir, Cure Elixir,
@@ -28,7 +28,6 @@
 
       <!-- Segunda linha (Divisão entre Surfe e Outros Itens) -->
       <div class="second-row">
-        <!-- Coluna do Surfe -->
         <div class="first-column">
           <div class="special-item">
             <q-img
@@ -36,7 +35,7 @@
               src="../../../assets/image/eg/guide/30_days_packet/mount_30d.png"
             />
             <div class="text">
-              <span class="name">Montaria de Prancha de Surfe 30D</span>
+              <span class="name">Surfboard Mount 30D</span>
               <p class="description">Surfe com estilo por 30 dias!</p>
             </div>
             <q-img
@@ -48,7 +47,6 @@
 
         <!-- Coluna dos Outros Itens -->
         <div class="second-column">
-          <!-- Outros Itens -->
           <div class="item">
             <q-img
               class="itens-img"
@@ -99,13 +97,13 @@
     </div>
 
     <!-- Seção do Pet -->
-    <div class="pet-section">
+    <div>
       <h6 class="pet-title">
         In addition to purchased items, there is a hidden/forgotten quest in the
         game that gives you a pet:
       </h6>
 
-      <div class="img-map-colun">
+      <div class="img-colun">
         <div class="img-item">
           <span class="text-center">AOL</span>
           <q-img
@@ -125,35 +123,27 @@
         continue the mission.
       </h6>
 
-      <div class="img-map-colun">
-        <div class="img-item">
-          <span class="text-center"> Vueltas Scout (AOL)</span>
+      <div class="img-colun img-item">
+        <span class="text-center"> Vueltas Scout </span>
 
-          <q-img
-            src="../../../assets/image/eg/guide/30_days_packet/vueltas_scout.png"
-          />
-        </div>
-
-        <div class="img-item">
-          <span class="text-center"> Dwarf Bandit (UOF)</span>
-          <q-img
-            src="../../../assets/image/eg/guide/30_days_packet/dwarf_bandit.png"
-          />
-        </div>
+        <q-img
+          class="quest-mob"
+          src="../../../assets/image/eg/guide/30_days_packet/vueltas_scout_group.png"
+        />
       </div>
 
-      <h6 class="pet-title">
+      <h6 class>
         The drop is a secret document with a order to execute
         <strong> machine </strong>.
       </h6>
-      <div class="dispel-potion">
-        <div class="item">
+      <div class="quest-pet">
+        <div class="pet-item">
           <q-img
-            class="dispel-potion-img"
+            class="img-drop"
             src="../../../assets/image/eg/guide/30_days_packet/secret_document.png"
           />
-          <div class="text">
-            <span class="name">Secret Document</span>
+          <div>
+            <strong>Secret Document</strong>
             <p class="description">
               Begins Quest<br />Right-Click the mouse button to start the quest.
             </p>
@@ -163,7 +153,7 @@
             </p>
           </div>
           <q-img
-            class="dispel-potion-img"
+            class="img-icon"
             src="../../../assets/image/eg/guide/30_days_packet/pet_secret_quest.png"
           />
         </div>
@@ -177,19 +167,19 @@
         (Maybe this is a member of his family lmao)
       </h6>
       <q-img
-        class=""
         src="../../../assets/image/eg/guide/30_days_packet/drekslur_the_watcher.png"
       />
 
       <h6 class="pet-title">
-        After completing the quest, you will be rewarded with a 15-day pet
-        called
-        <strong>MK-01 +5 (15d)</strong>
+        After completing the quest, you will be rewarded with a 1 day pet
+
+        <strong> +5 (1d)</strong>
         which is a robotic partner ready for the greatest adventures.
       </h6>
+
       <q-img
         class=""
-        src="../../../assets/image/eg/guide/30_days_packet/pet.png"
+        src="../../../assets/image/eg/guide/30_days_packet/robots_reward.png"
       />
     </div>
   </div>
@@ -202,58 +192,76 @@ export default {
 </script>
 
 <style scoped>
-.img-map-colun {
-  display: flex;
-  gap: 20px;
-}
-
-.img-title {
-  align-items: center;
-}
-.img-item {
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-}
-
 .container {
   padding: 20px;
   font-family: Arial, sans-serif;
   max-width: 800px;
-  margin: auto;
 }
-
 .title {
   font-size: 2em;
   text-align: center;
   margin-bottom: 10px;
 }
-
 .subtitle {
   font-size: 1em;
   margin-bottom: 20px;
 }
 
 .npc-image {
-  width: 100%;
-  max-width: 600px;
   border-radius: 10px;
-  margin: 0 auto 20px;
-}
-.board-image {
-  width: 100%;
-  max-width: 600px;
-  border-radius: 10px;
-  margin: 0 auto 20px;
+  margin-bottom: 20px;
 }
 .itens-wrapper {
   display: grid;
-  grid-template-columns: 1fr; /* Uma coluna inicialmente */
+  gap: 20px;
+}
+.item,
+.special-item {
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  background: #f5f5f5;
+}
+
+.item,
+.pet-item {
+  display: flex;
+  align-items: center;
+  gap: 15px;
+  border-radius: 8px;
+  padding: 15px;
+}
+
+.img-colun {
+  display: flex;
   gap: 20px;
 }
 
-.dispel-potion {
-  grid-column: span 1; /* O dispel potion ocupa toda a largura */
+.img-item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+
+.quest-pet {
+  display: flex;
+  gap: 20px;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+.quest-mob {
+  display: flex;
+  flex-direction: center;
+  width: 60%;
+  height: auto;
+}
+
+.img-drop {
+  width: 15%;
+}
+
+.board-image {
+  border-radius: 10px;
 }
 
 .second-row {
@@ -262,72 +270,40 @@ export default {
 }
 
 .first-column {
-  flex: 1; /* A coluna de Surfe ocupa a metade da largura */
+  flex: 1;
   display: flex;
   flex-direction: column;
-  gap: 10px;
+  gap: 20px;
 }
 
 .second-column {
-  flex: 1; /* A coluna dos outros itens ocupa a outra metade */
+  flex: 1;
   display: flex;
   flex-direction: column;
-  gap: 15px;
+  gap: 20px;
 }
 
 .special-item {
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 10px;
-  background: #f5f5f5;
+  gap: 20px;
   padding: 15px;
   border-radius: 8px;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-}
-
-.item,
-.pet-item {
-  display: flex;
-  align-items: center;
-  gap: 15px;
-  background: #f5f5f5;
-  padding: 15px;
-  border-radius: 8px;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
 }
 
 .itens-img {
   width: 32px;
+  flex-shrink: 0;
+}
+
+.img-icon {
+  width: 200px;
   height: auto;
-  object-fit: contain;
-  flex-shrink: 0; /* Evita que a imagem diminua em telas pequenas */
-}
-
-.dispel-potion-img {
-  width: 200px; /* Aumenta o tamanho da imagem do dispel potion */
-  height: auto;
-}
-
-.text {
-  display: flex;
-  flex-direction: column;
-}
-
-.name {
-  font-weight: bold;
-  font-size: 1em;
 }
 
 .description {
   font-size: 0.9em;
   color: #555;
-}
-
-.pet-title {
-  text-align: justify;
-  margin-bottom: 20px;
-  font-size: 1.2em;
-  color: #333;
 }
 </style>
