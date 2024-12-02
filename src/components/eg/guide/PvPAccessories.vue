3d2719cd1aebb09eac78bc6fc3348d129caca2a2
Dionatas Santos Brito <80075307+DsBrito@users.noreply.github.com>
2024-12-02 01:26:18 -0300

    :tada:  update accs guide

--- src/components/eg/guide/PvPAccessories.vue
+++ src/components/eg/guide/PvPAccessories.vue
@@ -244,150 +244,338 @@
         />
       </div>
     </div>
-  </div>
-  <div v-if="selectedTitle === 'PvP 60'" class="pvp-zone">
-    <h3 class="text-center no-margin">{{ selectedTitle }}</h3>
-    <!-- <p>Content for {{ selectedTitle }} PvP.</p> -->
+    <div v-if="selectedTitle === 'PvP 60'" class="pvp-zone">
+      <h3 class="text-center no-margin">{{ selectedTitle }}</h3>
+      <!-- <p>Content for {{ selectedTitle }} PvP.</p> -->
 
-    <!-- Description -->
-    <h6>
-      Texto pvp 60 Texto pvp 60 Texto pvp 60 Texto pvp 60 Texto pvp 60 Texto pvp
-      60 Texto pvp 60 Texto pvp 60 Texto pvp 60 Texto pvp 60
-    </h6>
-    <!-- TELEPORT -->
-    <q-img
-      class="img-teleport"
-      src="../../../assets/image/eg/guide/accessories/maps/60/teleport60.png"
-    />
-    <!-- Description -->
-    <h6>
-      Texto pvp 60 Texto pvp 60 Texto pvp 60 Texto pvp 60 Texto pvp 60 Texto pvp
-      60 Texto pvp 60 Texto pvp 60 Texto pvp 60
-    </h6>
-    <!-- MAP -->
-    <q-img
-      class="img-map"
-      src="../../../assets/image/eg/guide/accessories/maps/60/map60.png"
-    />
-    <!-- MOBS -->
-    <!-- Description -->
-    <h6>
-      Texto pvp 60 v Texto pvp 60 Texto pvp 60 Texto pvp 60 Texto pvp 60 Texto
-      pvp 60 Texto pvp 60 Texto pvp 60 Texto pvp 60 Texto pvp 60 Texto pvp 60
-      Texto pvp 60 s
-    </h6>
-    <!-- MAP -->
-    <div class="img-mobs">
-      <h1>COLOCAR MOBS NORMAIS AQUI (TODOS JUNTOS)</h1>
-      <q-img
-        src="../../../assets/image/eg/guide/accessories/mobs/30/burning-stone-dwarf-digger.png"
-      />
-      <q-img
-        src="../../../assets/image/eg/guide/accessories/mobs/30/demonStone_troll_guardian.png"
-      />
-      <q-img
-        src="../../../assets/image/eg/guide/accessories/mobs/30/drago-evil -spirit.png"
-      />
-      <q-img
-        src="../../../assets/image/eg/guide/accessories/mobs/30/drago-eye-medic.png"
-      />
-    </div>
-    <!-- Description -->
-    <h6>
-      Texto pvp 30Texto pvp 30Texto pvp 30Texto pvp 30Texto pvp 30Texto pvp
-      30Texto pvp 30Texto pvp 30Texto pvp 30Texto pvp 30Texto pvp 30Texto pvp
-      30Texto pvp 30Texto pvp 30
-    </h6>
-    <!-- MAP -->
-    <div class="img-mobs">
-      <h1>COLOCAR Mini BOSSS AQUI (TODOS JUNTOS)</h1>
-      <q-img
-        src="../../../assets/image/eg/guide/accessories/mobs/30/burning-stone-dwarf-digger.png"
-      />
-      <q-img
-        src="../../../assets/image/eg/guide/accessories/mobs/30/demonStone_troll_guardian.png"
-      />
-      <q-img
-        src="../../../assets/image/eg/guide/accessories/mobs/30/drago-evil -spirit.png"
-      />
-      <q-img
-        src="../../../assets/image/eg/guide/accessories/mobs/30/drago-eye-medic.png"
-      />
-    </div>
-  </div>
-  <div v-if="selectedTitle === 'PvP 80'" class="pvp-zone">
-    <h3 class="text-center no-margin">{{ selectedTitle }}</h3>
-    <!-- <p>Content for {{ selectedTitle }} PvP.</p> -->
-
-    <!-- Description -->
-    <h6>
-      EEE CONSEGUIDO ATRAVÉS DEEE ........... EEE CONSEGUIDO ATRAVÉS DEEE
-      ............. EEE CONSEGUIDO ATRAVÉS DEEE ....... EEE CONSEGUIDO ATRAVÉS
-      DEEE ...APULUNE/IRES INMAGEM
-    </h6>
-    <!-- Seção do Pet -->
-    <div>
-      <h6 class="pet-title">
-        In addition to purchased items, there is a hidden/forgotten quest in the
-        game that gives you a pet:
+      <!-- Description -->
+      <h6>
+        Texto pvp 60 Texto pvp 60 Texto pvp 60 Texto pvp 60 Texto pvp 60 Texto
+        pvp 60 Texto pvp 60 Texto pvp 60 Texto pvp 60 Texto pvp 60
       </h6>
-
-      <div class="img-colun">
-        <div class="img-item">
+      <!-- TELEPORT -->
+      <q-img
+        class="img-teleport"
+        src="../../../assets/image/eg/guide/accessories/maps/60/teleport60.png"
+      />
+      <!-- Description -->
+      <h6>
+        Texto pvp 60 Texto pvp 60 Texto pvp 60 Texto pvp 60 Texto pvp 60 Texto
+        pvp 60 Texto pvp 60 Texto pvp 60 Texto pvp 60
+      </h6>
+      <!-- MAP -->
+      <q-img
+        class="img-map"
+        src="../../../assets/image/eg/guide/accessories/maps/60/map60.png"
+      />
+      <!-- MOBS -->
+      <!-- Description -->
+      <h6>
+        Texto pvp 60 v Texto pvp 60 Texto pvp 60 Texto pvp 60 Texto pvp 60 Texto
+        pvp 60 Texto pvp 60 Texto pvp 60 Texto pvp 60 Texto pvp 60 Texto pvp 60
+        Texto pvp 60 s
+      </h6>
+      <!-- MAP -->
+      <div class="img-mobs">
+        <h1>COLOCAR MOBS NORMAIS AQUI (TODOS JUNTOS)</h1>
+        <q-img
+          src="../../../assets/image/eg/guide/accessories/mobs/30/burning-stone-dwarf-digger.png"
+        />
+        <q-img
+          src="../../../assets/image/eg/guide/accessories/mobs/30/demonStone_troll_guardian.png"
+        />
+        <q-img
+          src="../../../assets/image/eg/guide/accessories/mobs/30/drago-evil -spirit.png"
+        />
+        <q-img
+          src="../../../assets/image/eg/guide/accessories/mobs/30/drago-eye-medic.png"
+        />
+      </div>
+      <!-- Description -->
+      <h6>
+        Texto pvp 30Texto pvp 30Texto pvp 30Texto pvp 30Texto pvp 30Texto pvp
+        30Texto pvp 30Texto pvp 30Texto pvp 30Texto pvp 30Texto pvp 30Texto pvp
+        30Texto pvp 30Texto pvp 30
+      </h6>
+      <!-- MAP -->
+      <div class="img-mobs">
+        <h1>COLOCAR Mini BOSSS AQUI (TODOS JUNTOS)</h1>
+        <q-img
+          src="../../../assets/image/eg/guide/accessories/mobs/30/burning-stone-dwarf-digger.png"
+        />
+        <q-img
+          src="../../../assets/image/eg/guide/accessories/mobs/30/demonStone_troll_guardian.png"
+        />
+        <q-img
+          src="../../../assets/image/eg/guide/accessories/mobs/30/drago-evil -spirit.png"
+        />
+        <q-img
+          src="../../../assets/image/eg/guide/accessories/mobs/30/drago-eye-medic.png"
+        />
+      </div>
+    </div>
+    <div v-if="selectedTitle === 'PvP 80'" class="pvp-zone">
+      <h3 class="text-center no-margin">{{ selectedTitle }}</h3>
+      <!-- Description -->
+      <h6>
+        After completing the quest, you will be rewarded with a 1 day pet After
+        completing the quest, you will be rewarded with a 1 day pet After
+        completing the quest, you will be rewarded with a 1 day pet
+      </h6>
+      <div class="row justify-center q-gutter-lg text-center">
+        <div class="img-map">
           <span class="text-center">AOL</span>
           <q-img
             src="../../../assets/image/eg/guide/accessories/maps/80/apulune-accs-quest.png"
           />
         </div>
 
-        <div class="img-item">
+        <div class="img-map">
           <span class="text-center">UOF</span>
           <q-img
             src="../../../assets/image/eg/guide/accessories/maps/80/iris-accs-quest.png"
           />
         </div>
       </div>
-    </div>
-    <h6>
-      NPC...NPC...NPC...NPC...NPC...NPC...NPC...NPC...NPC...NPC....Texto pvp 80
-      Texto pvp 80 Texto pvp 80 Texto pvp 80 Texto pvp 80 Texto
-      pvNPC...NPC...NPC...NPC...NPC...NPC...NPC...NPC...NPC...NPC...NPC...NPC...NPC...NPC...NPC...NPC...NPC...NPC...p
-      80 Texto pvp 80 Texto pvp 80 Texto pvp 80 Texto pvp 80 Texto pvp 80 Texto
-      pvp 80 Texto pvp 80 Texto pvp 80
-    </h6>
-    <q-img
-      class="npc-quest"
-      src="../../../assets/image/eg/guide/accessories/npcs/npcs-acc-quest.png"
-    />
-    <h6>
-      Texto pvp 80 Texto pvp 80 Texto pvp 80 Texto pvp 80 Texto pvp 80 Texto pvp
-      80 Texto pvp 80 Texto pvp 80 Texto pvp 80 Texto pvp 80 Texto pvp 80 Texto
-      pvp 80 Texto pvp 80 Texto pvp 80
-    </h6>
-    <div>
-      <q-carousel
-        v-model="slide"
-        swipeable
-        animated
-        control-color="amber"
-        navigation
-        padding
-        arrows
-      >
-        <q-carousel-slide :name="1" class="no-padding">
-          <h6 class="no-margin">Text/Explain1</h6>
-          <q-img
-            src="../../../assets/image/eg/guide/accessories/npcs/anyquest.png"
-          />
-        </q-carousel-slide>
-        <!-- <q-carousel-slide :name="2" class="no-padding">
-        <h6 class="no-margin">Text/Explain2</h6>
-        <q-img
-          class=""
-          src="../../../assets/image/eg/guide/accessories/npcs/anyquest.png"
+
+      <h6>
+        After completing the quest, you will be rewarded with a 1 day pet After
+        completing the quest, you will be rewarded with a 1 day pet After
+        completing the quest, you will be rewarded with a 1 day pet
+      </h6>
+      <q-img
+        class="npc-quest"
+        src="../../../assets/image/eg/guide/accessories/npcs/all-npcs.png"
+      />
+      <h6>
+        After completing the quest, you will be rewarded with a 1 day pet After
+        completing the quest, you will be rewarded with a 1 day pet After
+        completing the quest, you will be rewarded with a 1 day pet
+      </h6>
+
+      <div class="img-quest">
+        <q-carousel
+          v-model="slide"
+          swipeable
+          animated
+          control-color="amber"
+          navigation
+          padding
+          arrows
+        >
+          <!-- Slide 1 -->
+          <q-carousel-slide :name="1" class="no-padding no-scroll">
+            <div class="flex-container">
+              <q-img
+                class="img-acc-quest"
+                src="../../../assets/image/eg/guide/accessories/accs/acc-enock-ghabiel.png"
+              />
+              <p class="text-center">
+                Enock Ghabiel (x100 Chaos Plate Fragment)
+              </p>
+
+              <q-img
+                class="img-acc-quest"
+                src="../../../assets/image/eg/guide/accessories/accs/acc-enock-ghabiel.png"
+              />
+            </div>
+            <!-- Container para as duas imagens -->
+            <div class="row items-center justify-around no-wrap">
+              <!-- Imagem 1 -->
+              <q-img
+                src="../../../assets/image/eg/guide/accessories/npcs/enock-ghabiel.png"
+              />
+              <!-- Imagem 2 -->
+              <q-img
+                src="../../../assets/image/eg/guide/accessories/npcs/quest-enock-ghabiel.png"
+              />
+            </div>
+          </q-carousel-slide>
+          <q-carousel-slide :name="2" class="no-padding no-scroll">
+            <div class="flex-container">
+              <q-img
+                class="img-acc-quest"
+                src="../../../assets/image/eg/guide/accessories/accs/acc-elenen-ghabiel.png"
+              />
+              <p class="text-center">
+                Elenen Ghabiel (x120 Chaos Plate Fragment)
+              </p>
+
+              <q-img
+                class="img-acc-quest"
+                src="../../../assets/image/eg/guide/accessories/accs/acc-elenen-ghabiel.png"
+              />
+            </div>
+            <!-- Container para as duas imagens -->
+            <div class="row items-center justify-around no-wrap">
+              <!-- Imagem 1 -->
+              <q-img
+                src="../../../assets/image/eg/guide/accessories/npcs/elenen-ghabiel.png"
+              />
+
+              <!-- Imagem 2 -->
+              <q-img
+                src="../../../assets/image/eg/guide/accessories/npcs/quest-elenen-ghabiel.png"
+              />
+            </div>
+          </q-carousel-slide>
+          <q-carousel-slide :name="3" class="no-padding no-scroll">
+            <div class="flex-container">
+              <q-img
+                class="img-acc-quest"
+                src="../../../assets/image/eg/guide/accessories/accs/acc-nole-torraine.png"
+              />
+              <p class="text-center">
+                Nole Torraine (x140 Chaos Plate Fragment)
+              </p>
+              <q-img
+                class="img-acc-quest"
+                src="../../../assets/image/eg/guide/accessories/accs/acc-nole-torraine.png"
+              />
+            </div>
+
+            <!-- Container para as duas imagens -->
+            <div class="row items-center justify-around no-wrap">
+              <!-- Imagem 1 -->
+              <q-img
+                src="../../../assets/image/eg/guide/accessories/npcs/nole-torraine.png"
+              />
+
+              <!-- Imagem 2 -->
+              <q-img
+                src="../../../assets/image/eg/guide/accessories/npcs/quest-nole-torraine.png"
+              />
+            </div>
+          </q-carousel-slide>
+          <q-carousel-slide :name="4" class="no-padding no-scroll">
+            <div class="flex-container">
+              <q-img
+                class="img-acc-quest"
+                src="../../../assets/image/eg/guide/accessories/accs/acc-patril-sibelan.png"
+              />
+              <p class="text-center">
+                Patril Siberlan (x100 Chaos Plate Fragment)
+              </p>
+              <q-img
+                class="img-acc-quest"
+                src="../../../assets/image/eg/guide/accessories/accs/acc-patril-sibelan.png"
+              />
+            </div>
+
+            <!-- Container para as duas imagens -->
+            <div class="row items-center justify-around no-wrap">
+              <!-- Imagem 1 -->
+              <q-img
+                src="../../../assets/image/eg/guide/accessories/npcs/patril-sibelan.png"
+              />
+              <!-- Imagem 2 -->
+              <q-img
+                src="../../../assets/image/eg/guide/accessories/npcs/quest-patril-sibelan.png"
+              />
+            </div>
+          </q-carousel-slide>
+          <q-carousel-slide :name="5" class="no-padding no-scroll">
+            <div class="flex-container">
+              <q-img
+                class="img-acc-quest"
+                src="../../../assets/image/eg/guide/accessories/accs/acc-cordelan-crane.png"
+              />
+              <p class="text-center">
+                Cordelan Crane (x120 Chaos Plate Fragment)
+              </p>
+
+              <q-img
+                class="img-acc-quest"
+                src="../../../assets/image/eg/guide/accessories/accs/acc-cordelan-crane.png"
+              />
+            </div>
+            <!-- Container para as duas imagens -->
+            <div class="row items-center justify-around no-wrap">
+              <!-- Imagem 1 -->
+              <q-img
+                src="../../../assets/image/eg/guide/accessories/npcs/cordelan-crane.png"
+              />
+              <!-- Imagem 2 -->
+              <q-img
+                src="../../../assets/image/eg/guide/accessories/npcs/quest-cordelan-crane.png"
+              />
+            </div>
+          </q-carousel-slide>
+        </q-carousel>
+      </div>
+
+      <img
+        class="q-pa-sm"
+        src="../../../assets/image/eg/guide/accessories/item/chaos_plate_fragment_description.png"
+      />
+      <h6>
+        You need farm this item:
+        <img
+          src="../../../assets/image/eg/guide/accessories/item/chaos_plate_fragment.png"
         />
-      </q-carousel-slide> -->
-      </q-carousel>
+        in Dwater or Panda/Theo
+      </h6>
+
+      <div class="row justify-center q-gutter-lg text-center">
+        <div class="img-teleport">
+          <span class="text-center">Teleport</span>
+          <q-img
+            src="../../../assets/image/eg/guide/accessories/maps/80/d_water.png"
+          />
+        </div>
+
+        <div class="img-map">
+          <span class="text-center">Map</span>
+          <q-img
+            src="../../../assets/image/eg/guide/accessories/maps/80/map_d_water.png"
+          />
+        </div>
+      </div>
+
+      <h6>
+        You need farm this item:
+        <img
+          src="../../../assets/image/eg/guide/accessories/item/chaos_plate_fragment.png"
+        />
+        in Dwater or Panda/Theo
+      </h6>
+      <div class="row justify-center q-gutter-lg text-center">
+        <div class="img-teleport">
+          <span class="text-center">Teleport</span>
+          <q-img
+            src="../../../assets/image/eg/guide/accessories/maps/80/d_water.png"
+          />
+        </div>
+
+        <div class="img-map">
+          <span class="text-center">Map</span>
+          <q-img
+            src="../../../assets/image/eg/guide/accessories/maps/80/cog-portal.png"
+          />
+        </div>
+      </div>
+
+      <h6>
+        After completing the quest, you will be rewarded with a 1 day pet After
+        completing the quest, you will be rewarded with a 1 day pet After
+        completing the quest, you will be rewarded with a 1 day pet
+      </h6>
+
+      <div class="row justify-center q-gutter-lg text-center">
+        <div class="img-map">
+          <span class="text-center">Pantanasa (AOL)</span>
+          <q-img
+            src="../../../assets/image/eg/guide/accessories/maps/80/map_pantanasa.png"
+          />
+        </div>
+
+        <div class="img-map">
+          <span class="text-center">Theodores (UOF)</span>
+          <q-img
+            src="../../../assets/image/eg/guide/accessories/maps/80/map_theodores.png"
+          />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -410,27 +598,30 @@ const selectTitle = (title: string | null) => {
   max-width: 80%;
   margin: auto;
 }
+.flex-container {
+  display: flex;
+  align-items: center;
+}
+.img-acc-quest {
+  width: 60px;
+}
 .image-style {
   width: 130px;
   height: auto;
   transform: rotate(90deg);
 }
 
-.img-colun {
-  display: flex;
-  gap: 20px;
+.img-map {
+  width: 400px;
 }
 
-.img-item {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
+.img-quest {
+  width: 450px;
 }
+
 .npc-quest {
-  width: 30%;
+  width: 45%;
   border-radius: 10px;
-  margin-bottom: 20px;
 }
 .title {
   font-size: 2em;
@@ -447,10 +638,10 @@ const selectTitle = (title: string | null) => {
 .item {
   display: flex;
   align-items: center;
-  gap: 15px;
+  gap: 10px;
   background: #f5f5f5;
-  padding: 15px;
-  min-width: 300px;
+  padding: 10px;
+  min-width: 270px;
   cursor: pointer;
   transition: transform 0.2s ease-in-out;
 }
@@ -475,11 +666,9 @@ const selectTitle = (title: string | null) => {
 }
 
 .img-teleport {
-  width: 20%;
+  width: 200px;
 }
-.img-map {
-  width: 30%;
-}
+
 .img-mobs {
   width: 100%;
   display: flex; /* Coloca os itens em uma linha */
